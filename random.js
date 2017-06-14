f3054cdbb7436c5abc01da2370434a7e642c5a70
zhangzhenhui <924393956@qq.com>
2017-06-14 14:41:27 +0800

    加注释

--- random.js
+++ random.js
@@ -22,27 +22,35 @@ var random = function() {
         .concat(loop(male, Math.ceil(male.length / size)));
     }
 
-    // 可以强制按每组多少人来计算，而不区分性别
+    // 可以强制按每组多少人来计算，而不区分性别,那需要分的组数为:Math.ceil(team.length / size)
     if(isForceSizeAsMemberCountInGroup) size = Math.ceil(team.length / size);
 
-    var females = loop(female, size);
-    var males = loop(male, size).reverse();
+    var females = loop(female, size);//把女生随机分成size组
+    var males = loop(male, size).reverse();//把男生随机分成size组
 
+    /**
+     * 这里把males和females的值对换，为了下面map遍历females的时候是遍历长度长的一组性别，
+     * 合并数组的时候能覆盖短的一组，短的不够的部分作为为[]合并
+     */
     if(females.length - males.length < 0) females = [males, males = females][0];
     return females.map(function(group, i) {
-      return group.concat(males[i] || []);
+      return group.concat(males[i] || []);//
     });
   }
 
+  /**
+   * arr要分组的群体
+   * size全体要分成的组数
+   */
   function loop(arr, size){
     var i = 0;
     var result = [];
 
-    while(arr.length) {
+    while(arr.length) {//当数组元素都分出去后，长度为0
       var index = Math.floor(Math.random() * arr.length);
       if(!result[i]) result[i] = [];
-      result[i].push(arr.splice(index, 1)[0]);
-      if(++i === size) i = 0;
+      result[i].push(arr.splice(index, 1)[0]);//往i组分一个成员
+      if(++i === size) i = 0;//当i===size时，表示经过一次循环往size组中每组分了一个，把i=0,继续再次循环分成员，知道循环结束
     }
 
     return result;
